fc8170fb0cf47491ed068786ef2381d3ff6d1df3
Hanwei <hanwei.shao@outlook.com>
2015-07-05 14:06:49 +0930

    sentiment

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -17,4 +17,17 @@ class InfoController < ApplicationController
 		puts @labelAry
 	end	
 	
+	def news
+			url = "http://unleashedss.herokuapp.com/api/index?country=china"
+            jsonStr = Net::HTTP.get_response(URI.parse(url))            
+            data = jsonStr.body
+            
+            jsonHash = JSON.parse(data)
+			@score = jsonHash["totalScore"] * 2
+			@newsAry = jsonHash["resultset"]
+			
+			
+			#@score = 3.64904866102933805 * 2
+	end
+	
 end
