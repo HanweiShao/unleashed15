cb251b8b6c8c0fa2419b6c49cf42a97547c2a34a
Hanwei <hanwei.shao@outlook.com>
2015-07-05 14:48:28 +0930

    controller

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -33,4 +33,21 @@ class InfoController < ApplicationController
 			#@score = 3.64904866102933805 * 2
 	end
 	
+	def news
+			@country = params[:c]
+			url = "http://unleashedss.herokuapp.com/api/index?country=#{@country}"
+            jsonStr = Net::HTTP.get_response(URI.parse(url))            
+            data = jsonStr.body
+            
+			puts jsonStr
+			
+            jsonHash = JSON.parse(data)
+			@score = jsonHash["totalScore"] * 2
+			@newsAry = jsonHash["resultset"]
+			@recommandAry = jsonHash["recomendations"]
+			
+			
+			#@score = 3.64904866102933805 * 2
+	end
+	
 end
